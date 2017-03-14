418cfc96dee6874d8e32d6bc4ac53512409cb4e6
Xavier BOUCLET <mikrethor@gmail.com>
2017-03-13 22:18:31 -0400

    Adding LoginController junit

--- src/test/java/fr/ablx/daycare/controllers/LoginControllerTest.java
+++ src/test/java/fr/ablx/daycare/controllers/LoginControllerTest.java
@@ -1,34 +1,48 @@
 package fr.ablx.daycare.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import fr.ablx.daycare.config.Application;
 import fr.ablx.daycare.jpa.User;
 import fr.ablx.daycare.objects.Credentials;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {LoginController.class })
+@ContextConfiguration(classes = {Application.class})
 @WebAppConfiguration
 public class LoginControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private CommandLineRunner demo;
+
+    @Autowired
     private WebApplicationContext wac;
 
+    @Autowired
+    private ObjectMapper mapperJsonObject;
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        demo.run();
         mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
@@ -39,18 +53,24 @@ public class LoginControllerTest {
 
     @Test
     public void login() throws Exception {
+
         Credentials user = new Credentials();
         user.setLogin("test@test2");
         user.setPassword("test");
 
-       // String json = new Gson().toJson(user);
+        String json = new Gson().toJson(user);
 
-        mockMvc.perform(
-                post("/project")
+        ResultActions resultActions = mockMvc.perform(
+                post("/login")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(""))
-                .andExpect(status().isCreated());
+                        .content(json))
+                .andExpect(status().isOk());
+        String result = resultActions.andReturn().getResponse().getContentAsString();
+
+
+        User u = mapperJsonObject.readValue(result, User.class);
+        Assert.assertEquals("{\"id\":3,\"login\":\"test@test2\",\"daycareId\":1,\"educator\":null,\"parent\":null,\"admin\":1}", resultActions.andReturn().getResponse().getContentAsString());
     }
 
     @Test
